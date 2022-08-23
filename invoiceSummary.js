ac819bd0dbf6899a2c00e6a15ae2d05a1d4631c8
ShivaSaiMudiraj <108717696+ShivaSaiMudiraj@users.noreply.github.com>
2022-08-23 19:00:07 +0530

    Update invoiceSummary.js

--- invoiceSummary.js
+++ invoiceSummary.js
@@ -8,15 +8,19 @@ export default class InvoiceSummary extends LightningElement {
     error;
     invoices;
     invdata=[];
+    overduedata=[];
+    count1to30=0;
+    count31to60=0;
+    count61to90=0;
+    count91=0;
+    countStatus=0;
     @api recordId;
-    OverdueInvoicesString;
     
     @track columns = [
         {
         label: 'Invoice #',
         fieldName: 'invoiceName',
         type: 'url',
-        sortable: true,
         typeAttributes: {label: { fieldName: 'Name' }, target: '_blank'}
     },
     {
@@ -33,38 +37,32 @@ export default class InvoiceSummary extends LightningElement {
     {
         label: 'Invoice Date',
         fieldName: 'Invoice_Date__c',
-        type: 'Date',
-        sortable: true
+        type: 'Date'
     },
     {
         label: 'Due Date',
         fieldName: 'Due_Date__c',
-        type: 'Date',
-        sortable: true
+        type: 'Date'
     },
     {
         label: 'Amount Paid',
         fieldName: 'Amount_Paid__c',
-        type: 'currency',
-        sortable: true
+        type: 'currency'
     },
     {
         label: 'Amount Due',
         fieldName: 'Amount_Due__c',
-        type: 'currency',
-        sortable: true
+        type: 'currency'
     },
     {
         label: 'Days Overdue',
         fieldName: 'Days_Overdue__c',
-        type: 'number',
-        sortable: true
+        type: 'number'
     },
     {
         label: 'Total',
         fieldName: 'Total__c',
-        type: 'currency',
-        sortable: true
+        type: 'currency'
     }
     
 ];
@@ -87,27 +85,36 @@ wiredInvoices({ error, data }) {
         this.error = result.error;
     }
 }
+
 @wire(paidInvoices,  { AccountId:'$recordId' })TotalPaidInvoices;
 @wire(currentDueInvoices,  { AccountId:'$recordId' })CurrentDueInvoices;
 @wire(overdueInvoices,  { AccountId:'$recordId' })
 wiredOverdueInvoices({ error, data }) {
 if (data) 
-{
-    if(data >=1 && data<=30){
-        this.OverdueInvoicesString='1-30';
+{ 
+    this.overduedata = data;
+    this.countStatus= this.overduedata.length;
+    console.log('countStatus'+this.overduedata.length);
+    for(let i=0; i<this.overduedata.length; i++){
+        console.log('days overdue'+this.overduedata[i].Days_Overdue__c);
+       
+    if(this.overduedata[i].Days_Overdue__c >=1 && this.overduedata[i].Days_Overdue__c<=30){
+        console.log('count1to30==='+this.count1to30);
+         this.count1to30= this.count1to30 + 1;
     }
-    else if (data >=31 && data<=60) {
-        this.OverdueInvoicesString='31-60';
+    else if(this.overduedata[i].Days_Overdue__c >=31 && this.overduedata[i].Days_Overdue__c<=60){
+        console.log('count31to60==='+this.count31to60);
+         this.count31to60= this.count31to60 + 1;
     }
-    else if (data >=61 && data<=90) {
-        this.OverdueInvoicesString='61-90';
-    }
-    else if (data >=91) {
-        this.OverdueInvoicesString='91+';
-    }
-    else if (error) {
-    this.error = result.error;
-    }
+    else if(this.overduedata[i].Days_Overdue__c >=61 && this.overduedata[i].Days_Overdue__c <=90){
+        console.log('count61to90==='+this.count61to90);
+         this.count61to90= this.count61to90 + 1;
+    } 
+    else if(this.overduedata[i].Days_Overdue__c >=91){
+        console.log('count91==='+this.count91);
+         this.count91= this.count91 + 1;
+    }     
+}
 }
 }
 @wire(totalReceivablesInvoices,  { AccountId:'$recordId' })totalReceivables;
